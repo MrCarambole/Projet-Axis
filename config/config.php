4d63a89211a051340f40d85943041a2058868edb
Carambole <carambole258@gmail.com>
2023-12-19 14:30:50 +0100

    push de git hub

---
+++ config/config.php
@@ -0,0 +1,22 @@
+<?php
+$CONFIG = array (
+  'instanceid' => 'oc6yo4nhqwho',
+  'passwordsalt' => 'uJh87fktzmpoXAG34VEjpwfS3pUNDi',
+  'secret' => 'WKiJpHko8XlpGcW2h8/gmai5BJeMAGvaF9Gdnt8pKdbdzqpj',
+  'trusted_domains' => 
+  array (
+    0 => '192.168.1.22',
+  ),
+  'datadirectory' => '/var/www/nextcloud/data',
+  'dbtype' => 'mysql',
+  'version' => '27.1.3.2',
+  'overwrite.cli.url' => 'http://10.0.2.15',
+  'dbname' => 'nextcloud',
+  'dbhost' => 'localhost',
+  'dbport' => '',
+  'dbtableprefix' => 'oc_',
+  'mysql.utf8mb4' => true,
+  'dbuser' => 'user',
+  'dbpassword' => 'pommedeterre12352',
+  'installed' => true,
+);
