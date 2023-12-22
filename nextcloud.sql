43d708dcc53d2130365cff7a535b08bbce6f719f
MrCarambole <131371657+MrCarambole@users.noreply.github.com>
2023-12-22 13:39:56 +0100

    Add files via upload

---
+++ nextcloud.sql
@@ -0,0 +1,115 @@
+DROP TABLE IF EXISTS `oc_vcategory`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `oc_vcategory` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `uid` varchar(64) NOT NULL DEFAULT '',
+  `type` varchar(64) NOT NULL DEFAULT '',
+  `category` varchar(255) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`),
+  KEY `uid_index` (`uid`),
+  KEY `type_index` (`type`),
+  KEY `category_index` (`category`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `oc_vcategory`
+--
+
+LOCK TABLES `oc_vcategory` WRITE;
+/*!40000 ALTER TABLE `oc_vcategory` DISABLE KEYS */;
+/*!40000 ALTER TABLE `oc_vcategory` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `oc_vcategory_to_object`
+--
+
+DROP TABLE IF EXISTS `oc_vcategory_to_object`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `oc_vcategory_to_object` (
+  `objid` bigint(20) unsigned NOT NULL DEFAULT 0,
+  `categoryid` bigint(20) unsigned NOT NULL DEFAULT 0,
+  `type` varchar(64) NOT NULL DEFAULT '',
+  PRIMARY KEY (`categoryid`,`objid`,`type`),
+  KEY `vcategory_objectd_index` (`objid`,`type`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `oc_vcategory_to_object`
+--
+
+LOCK TABLES `oc_vcategory_to_object` WRITE;
+/*!40000 ALTER TABLE `oc_vcategory_to_object` DISABLE KEYS */;
+/*!40000 ALTER TABLE `oc_vcategory_to_object` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `oc_webauthn`
+--
+
+DROP TABLE IF EXISTS `oc_webauthn`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `oc_webauthn` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `uid` varchar(64) NOT NULL,
+  `name` varchar(64) NOT NULL,
+  `public_key_credential_id` varchar(255) NOT NULL,
+  `data` longtext NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `webauthn_uid` (`uid`),
+  KEY `webauthn_publicKeyCredentialId` (`public_key_credential_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `oc_webauthn`
+--
+
+LOCK TABLES `oc_webauthn` WRITE;
+/*!40000 ALTER TABLE `oc_webauthn` DISABLE KEYS */;
+/*!40000 ALTER TABLE `oc_webauthn` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `oc_whats_new`
+--
+
+DROP TABLE IF EXISTS `oc_whats_new`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `oc_whats_new` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `version` varchar(64) NOT NULL DEFAULT '11',
+  `etag` varchar(64) NOT NULL DEFAULT '',
+  `last_check` int(10) unsigned NOT NULL DEFAULT 0,
+  `data` longtext NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `version` (`version`),
+  KEY `version_etag_idx` (`version`,`etag`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `oc_whats_new`
+--
+
+LOCK TABLES `oc_whats_new` WRITE;
+/*!40000 ALTER TABLE `oc_whats_new` DISABLE KEYS */;
+/*!40000 ALTER TABLE `oc_whats_new` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-12-22 13:19:06
